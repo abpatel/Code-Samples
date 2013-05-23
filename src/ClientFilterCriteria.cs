d44f50cf8c22c8b8216043557c5a91606c9075a4
Abhijeet P <patelabhijeet@yahoo.com>
2013-05-22 23:46:28 -0700

    FluentAPI first cut

--- src/ClientFilterCriteria.cs
+++ src/ClientFilterCriteria.cs
@@ -6,7 +6,7 @@ namespace ConsoleClient
         Ascending = 0,
         Descending
     }
-    
+
     public class OrderBy
     {
         public string AttributeName { get; set; }
@@ -29,115 +29,226 @@ namespace ConsoleClient
 
     public class CriteriaBuilder<T> where T : class,new()
     {
-        T entityFilter = default(T);
+        public class ThenOrderBy
+        {
+            OrderByCriteria orderby;
+            public ThenOrderBy(OrderByCriteria orderby)
+            {
+                this.orderby = orderby;
+            }
+
+            public OrderByCriteria ThenBy
+            {
+                get
+                {
+                    return orderby;
+                }
+            }
+        }
+
+        public class OrderByCriteria
+        {
+            CriteriaBuilder<T> criteria;
+            ThenOrderBy thenorderby;
+            public OrderByCriteria(CriteriaBuilder<T> criteria)
+            {
+                this.criteria = criteria;
+                this.thenorderby = new ThenOrderBy(this);
+            }
+
+            private void AddOrder(Expression<Func<T, object>> expr, OrderByDirection direction)
+            {
+                PropertyInfo propInfo = null;
+                MemberExpression memberExpr = expr.Body as MemberExpression;
+                if (memberExpr != null)
+                {
+                    propInfo = memberExpr.Member as PropertyInfo;
+                }
+                else
+                {
+                    propInfo = (((UnaryExpression)expr.Body).Operand as MemberExpression).Member as PropertyInfo;
+                }
+                if (propInfo != null)
+                {
+                    var orderBy = this.criteria.orderbys.SingleOrDefault(o => o.AttributeName == propInfo.Name);
+                    if (orderBy != null)
+                    {
+                        orderBy.Direction = direction;
+                    }
+                    else
+                    {
+                        this.criteria.orderbys.Add(new OrderBy { AttributeName = propInfo.Name, Direction = direction });
+                    }
+                }
+            }
+
+            public ThenOrderBy Ascending(Expression<Func<T, object>> expr)
+            {
+                AddOrder(expr, OrderByDirection.Ascending);
+                return thenorderby;
+            }
+            public ThenOrderBy Descending(Expression<Func<T, object>> expr)
+            {
+                AddOrder(expr, OrderByDirection.Ascending);
+                return thenorderby;
+            }
+        }
+
+        public class AndCondition
+        {
+            CriteriaBuilder<T> criteria;
+            public AndCondition(CriteriaBuilder<T> criteria)
+            {
+                this.criteria = criteria;
+            }
+
+            public FilterCriteria And
+            {
+                get
+                {
+                    return criteria.Where;
+                }
+            }
+
+            public OrderByCriteria OrderBy
+            {
+                get
+                {
+                    return criteria.OrderBy;
+                }
+            }
+
+        }
+
+        public class FilterCriteria
+        {
+            CriteriaBuilder<T> criteria;
+            AndCondition andCondition;
+            public FilterCriteria(CriteriaBuilder<T> criteria)
+            {
+                this.criteria = criteria;
+                this.andCondition = new AndCondition(criteria);
+            }
+
+            public AndCondition ConditionIs(Expression<Func<T, object>> filter)
+            {
+                if (criteria.entityFilter == null)
+                {
+                    criteria.entityFilter = new T();
+                }
+                UnaryExpression eprx = filter.Body as UnaryExpression;
+                if (eprx != null)
+                {
+                    BinaryExpression xpr = eprx.Operand as BinaryExpression;
+                    if (xpr.NodeType == ExpressionType.Equal)
+                    {
+                        PropertyInfo propInfo = (xpr.Left as MemberExpression).Member as PropertyInfo;
+                        object value = (xpr.Right as ConstantExpression).Value;
+                        if (value != null)
+                        {
+                            propInfo.SetValue(criteria.entityFilter, value);
+                            if (!criteria.filterAttributes.Contains(propInfo.Name))
+                            {
+                                criteria.filterAttributes.Add(propInfo.Name);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Only equality comparisons are supported");
+                    }
+                }
+                return this.andCondition;
+            }
+        }
+
+        public class SelectCriteria
+        {
+            CriteriaBuilder<T> criteria;
+
+            public SelectCriteria(CriteriaBuilder<T> criteria)
+            {
+                this.criteria = criteria;
+            }
+            public SelectCriteria Attribute(Expression<Func<T, object>> expr)
+            {
+                PropertyInfo propInfo = null;
+                MemberExpression memberExpr = expr.Body as MemberExpression;
+                if (memberExpr != null)
+                {
+                    propInfo = memberExpr.Member as PropertyInfo;
+                }
+                else
+                {
+                    propInfo = (((UnaryExpression)expr.Body).Operand as MemberExpression).Member as PropertyInfo;
+                }
+                if (propInfo != null)
+                {
+                    var attr = this.criteria.attributesToLoad.SingleOrDefault(a => a == propInfo.Name);
+                    if (attr == null)
+                    {
+                        this.criteria.attributesToLoad.Add(propInfo.Name);
+                    }
+                }
+                return this;
+            }
+
+            public FilterCriteria Where
+            {
+                get
+                {
+                    return criteria.Where;
+                }
+            }
+
+            public OrderByCriteria OrderBy
+            {
+                get
+                {
+                    return criteria.OrderBy;
+                }
+            }
+        }
+
         List<string> attributesToLoad = new List<string>();
+        T entityFilter = default(T);
         List<OrderBy> orderbys = new List<OrderBy>();
         int? skip = null;
         int? take = null;
         List<string> filterAttributes = new List<string>();
+        SelectCriteria select;
+        FilterCriteria where;
+        OrderByCriteria order;
 
-        public CriteriaBuilder<T> AddFilter(Expression<Func<T, object>> filter)
+        public CriteriaBuilder()
         {
-            if (entityFilter == null)
-            {
-                entityFilter = new T();
-            }
-            UnaryExpression eprx = filter.Body as UnaryExpression;
-            if (eprx != null)
-            {
-                BinaryExpression xpr = eprx.Operand as BinaryExpression;
-                if (xpr.NodeType == ExpressionType.Equal)
-                {
-                    PropertyInfo propInfo = (xpr.Left as MemberExpression).Member as PropertyInfo;
-                    object value = (xpr.Right as ConstantExpression).Value;
-                    if (value != null)
-                    {
-                        propInfo.SetValue(entityFilter, value);
-                        if (!filterAttributes.Contains(propInfo.Name))
-                        {
-                            filterAttributes.Add(propInfo.Name);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new InvalidOperationException("Only equality comparisons are supported");
-                }
-            }
-            return this;
+            this.select = new SelectCriteria(this);
+            this.where = new FilterCriteria(this);
+            this.order = new OrderByCriteria(this);
         }
 
-        public CriteriaBuilder<T> LoadAttribute(Expression<Func<T,object>> expr)            
+        public SelectCriteria Load
         {
-            PropertyInfo propInfo = null;
-            MemberExpression memberExpr = expr.Body as MemberExpression;
-            if (memberExpr != null)
+            get
             {
-                propInfo = memberExpr.Member as PropertyInfo;
-            }
-            else
-            {
-                propInfo = (((UnaryExpression)expr.Body).Operand as MemberExpression).Member as PropertyInfo;
-            }
-            if (propInfo != null)
-            {
-               var attr = attributesToLoad.SingleOrDefault(a => a == propInfo.Name);
-               if (attr == null)
-               {
-                   attributesToLoad.Add(propInfo.Name);
-               }
-            }
-            return this;
-        }
-
-        public void AddOrderBy(Expression<Func<T, object>> expr, OrderByDirection direction)
-        {
-            PropertyInfo propInfo = null;
-            MemberExpression memberExpr = expr.Body as MemberExpression;
-            if (memberExpr != null)
-            {
-                propInfo = memberExpr.Member as PropertyInfo;
-            }
-            else
-            {
-                propInfo = (((UnaryExpression)expr.Body).Operand as MemberExpression).Member as PropertyInfo;
-            }
-            if (propInfo != null)
-            {
-                var orderBy = orderbys.SingleOrDefault(o => o.AttributeName == propInfo.Name);
-                if (orderBy != null)
-                {
-                    orderBy.Direction = direction;
-                }
-                else
-                {
-                    this.orderbys.Add(new OrderBy { AttributeName = propInfo.Name, Direction = direction });
-                }
+                return select;
             }
         }
 
-        public CriteriaBuilder<T> OrderByDescending(Expression<Func<T, object>> expr)
+        public FilterCriteria Where
         {
-            AddOrderBy(expr, OrderByDirection.Descending);
-            return this;
+            get
+            {
+                return where;
+            }
         }
 
-        public CriteriaBuilder<T> OrderByAscending(Expression<Func<T, object>> expr)
+        public OrderByCriteria OrderBy
         {
-            AddOrderBy(expr, OrderByDirection.Ascending);
-            return this;
-        }
-
-        public CriteriaBuilder<T> Skip(int skip)
-        {
-            this.skip = skip;
-            return this;
-        }
-
-        public CriteriaBuilder<T> Take(int take)
-        {
-            this.take = take;
-            return this;
+            get
+            {
+                return order;
+            }
         }
 
         private string GetFilterStringForAttribute(string attribute)
@@ -151,22 +262,23 @@ namespace ConsoleClient
         public string GetCriteriaString()
         {
             StringBuilder sb = new StringBuilder();
+            sb.Append("?");
             if (attributesToLoad.Count > 0)
             {
-                string select = string.Concat("$select=", 
-                    attributesToLoad.Aggregate((s1, s2) => string.Format("{0},{1}", s1, s2)),"&");
+                string select = string.Concat("$select=",
+                    attributesToLoad.Aggregate((s1, s2) => string.Format("{0},{1}", s1, s2)), "&");
                 sb.Append(select);
             }
             if (filterAttributes.Count > 0)
             {
                 string filter = string.Concat("$filter=",
-                    filterAttributes.Aggregate((s1,s2) => string.Format("{0},{1}",GetFilterStringForAttribute(s1),GetFilterStringForAttribute(s2))),"&");
+                    filterAttributes.Aggregate((s1, s2) => string.Format("{0},{1}", GetFilterStringForAttribute(s1), GetFilterStringForAttribute(s2))), "&");
                 sb.Append(filter);
             }
             if (orderbys.Count > 0)
             {
                 string orderBy = string.Concat("$orderby=",
-                    orderbys.Select(o => string.Format("{0} {1}",o.AttributeName, (o.Direction == OrderByDirection.Ascending) ? "asc" : "desc"))
+                    orderbys.Select(o => string.Format("{0} {1}", o.AttributeName, (o.Direction == OrderByDirection.Ascending) ? "asc" : "desc"))
                             .Aggregate((s1, s2) => string.Format("{0},{1}", s1, s2)), "&");
                 sb.Append(orderBy);
             }
@@ -186,13 +298,6 @@ namespace ConsoleClient
         }
     }
 
-    public class Product
-    {
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public string Category { get; set; }
-    }
-    
     public class Proxy
     {
         Uri baseuri = null;
@@ -214,17 +319,25 @@ namespace ConsoleClient
         }
     }
 
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             CriteriaBuilder<Product> builder = new CriteriaBuilder<Product>();
-            builder
-             .AddFilter(x => x.Name == "test")
-             .AddFilter(x => x.Price == 100)
-             .LoadAttribute(x => x.Name)
-             .OrderByAscending(x => x.Name)
-             .OrderByDescending(x => x.Category);
+            builder.Load.Attribute(x => x.Name)
+                        .Attribute(x => x.Category)
+                    .Where.ConditionIs(x => x.Name == "test")
+                    .And.ConditionIs(x => x.Category == "test")
+                    .And.ConditionIs(x => x.Price == 100)
+                    .OrderBy.Ascending(x => x.Price)
+                    .ThenBy.Descending(x => x.Category);
             var criteria = builder.Build();
 
             Proxy proxy = new Proxy("http://locahost:2311/api/");
